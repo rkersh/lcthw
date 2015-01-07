be7afc9cdd33a1fdf761df82e011839d70f3aade
Ryan Kersh <ryankersh@gmail.com>
2015-01-07 13:25:17 -0800

    ex12 extra credit

--- ex12.c
+++ ex12.c
@@ -5,11 +5,14 @@ int main(int argc, char *argv[])
   int i = 0;
 
   if (argc == 1) {
+    /* argv[0] is always the name of the program. */
+    printf("You didn't provide any arguments.\n");
+  } else if (argc == 2) {
     printf("You only have one argument.  You suck.\n");
-  } else if (argc > 1 && argc < 4) {
+  } else if (argc > 2 && argc <= 4) {
     printf("Here's your arguments:\n");
 
-    for (i = 0; i < argc; i++) {
+    for (i = 1; i < argc; i++) {
       printf("%s ", argv[i]);
     }
     printf("\n");
